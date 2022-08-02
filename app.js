fed96c15aedf0674fe182ddaaa7da001d65e7430
Andre Chaves <cchaves.andre@gmail.com>
2022-08-02 00:44:54 -0300

    Middlewares

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
- app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas :)</h1)');
- })
+app.use(express.json());
 
- app.get('/json', (req, res) => {
+const log = (req, res, next) => {
+   console.log(req.body);
+   console.log(`Data: ${Date.now()}`);
+   next();
+}
+
+app.use(log);
+
+app.get('/', (req, res) => {
+   res.send('<h1>Minha lista de tarefas :)</h1)');
+})
+
+app.get('/json', (req, res) => {
    res.json({title: 'Tarefa X', done: true});
 })
 
- app.listen(3000, () => {
-    console.log('Servidor foi iniciado');
- })
+app.listen(3000, () => {
+   console.log('Servidor foi iniciado');
+})
